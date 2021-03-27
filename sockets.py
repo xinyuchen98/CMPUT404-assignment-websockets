48a7901958c370d74102451e5ed3b3f4365beb14
xinyuchen98 <xinyuchen98@gmail.com>
2021-03-27 16:09:47 -0600

    Added class client

--- sockets.py
+++ sockets.py
@@ -26,6 +26,17 @@ app = Flask(__name__)
 sockets = Sockets(app)
 app.debug = True
 
+class Client:
+    def __init__(self):
+        self.queue = queue.Queue()
+
+    def put(self, v):
+        self.queue.put_nowait(v)
+
+    def get(self):
+        return self.queue.get()
+
+
 class World:
     def __init__(self):
         self.clear()
